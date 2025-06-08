b7e78b030e40c90562372063ed6d9cab6da23307
fahdchalladia <fahdchalladia2004@gmail.com>
2025-06-08 15:56:50 +0100

    adding module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and print the contents of a UTF-8 text file.
+"""
+
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
