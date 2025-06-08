bedbf5d87007783fed68749ab652f2029be3c71a
fahdchalladia <fahdchalladia2004@gmail.com>
2025-06-08 16:01:28 +0100

    pep8 verified

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module provides a function to read and print the contents of a UTF-8 text file.
+This module provides a function to read&print the contents of a UTF-8 txt file.
 """
+
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content, end="")
-
