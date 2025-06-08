c51d07579f284b865305bc6570bdc83f3300610e
fahdchalladia <fahdchalladia2004@gmail.com>
2025-06-08 17:38:06 +0100

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""
-Script that adds all command-line argsto a list and saves them in a JSON file.
-"""
+"""Script that adds arguments to a list and saves it in a JSON file."""
 
 import sys
 import os
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 if os.path.exists(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
 
 my_list.extend(sys.argv[1:])
-
 save_to_json_file(my_list, filename)
