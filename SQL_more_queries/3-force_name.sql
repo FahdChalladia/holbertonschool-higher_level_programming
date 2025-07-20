f2a606b28d0be83bae04d746150341f7e007033d
fahdchalladia <fahdchalladia2004@gmail.com>
2025-07-20 20:22:54 +0100

    task3

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- Creates the table 'force_name' if it doesn't already exist
+-- The column 'name' cannot be NULL
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
