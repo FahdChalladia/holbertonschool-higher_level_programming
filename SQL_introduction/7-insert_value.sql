f066dae90784238082d69b4288684e952d618d17
fahdchalladia <fahdchalladia2004@gmail.com>
2025-07-20 16:08:04 +0100

    missing task code

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Lists all records in the table first_table
-SELECT * FROM first_table;
+-- Insert id 89 only if it doesn't already exist
+INSERT IGNORE INTO first_table (id, name)
+VALUES (89, 'Best School');
