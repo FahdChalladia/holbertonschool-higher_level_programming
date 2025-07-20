103b1c4e4e5482a07e62e3c21839b239835942b7
fahdchalladia <fahdchalladia2004@gmail.com>
2025-07-20 20:22:43 +0100

    task2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Creates the database 'hbtn_0d_2' if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creates the user 'user_0d_2' with password 'user_0d_2_pwd' if not already existing
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grants only SELECT privilege on the database 'hbtn_0d_2' to 'user_0d_2'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
