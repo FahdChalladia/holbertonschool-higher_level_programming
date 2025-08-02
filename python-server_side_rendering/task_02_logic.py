2450dea5e59607af9947d1b4538389a33d7577d9
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-02 11:15:11 +0100

    task 2 files

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    json_path = os.path.join(os.path.dirname(__file__), 'items.json')
+    with open(json_path) as f:
+        data = json.load(f)
+        item_list = data.get('items', [])
+    return render_template('items.html', items=item_list)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
