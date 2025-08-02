718cbcd6cefda4e132b8cb2b01c5f2c000e01bb1
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-02 12:24:04 +0100

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
+"""
+Displays all values in the `states` table where name matches the argument
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
-
     cur = db.cursor()
-    state_name = sys.argv[4]
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cur.execute(query)
     rows = cur.fetchall()
