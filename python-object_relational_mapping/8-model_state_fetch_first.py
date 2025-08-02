b6089c3b0714ee5da1754653c1cc9e8cea97545b
fahdchalladia <fahdchalladia2004@gmail.com>
2025-08-02 12:41:43 +0100

    task8 file

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Print the first State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn_str = (
+        f"mysql+mysqldb://{username}:"
+        f"{password}@localhost:3306/"
+        f"{database}"
+    )
+    engine = create_engine(conn_str, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
